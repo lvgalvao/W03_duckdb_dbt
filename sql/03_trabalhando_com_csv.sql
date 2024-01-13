69a49a6533c7c36cf7f614329f5c50e729905151
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-13 03:52:40 -0300

    feat(exercicio03): adicionar trabalhando com csv

---
+++ sql/03_trabalhando_com_csv.sql
@@ -0,0 +1,4 @@
+SELECT * FROM read_csv_auto('./data/csv/daily_sales_retail_0.csv');
+show tables;
+CREATE TABLE transaction AS FROM read_csv_auto('./data/csv/daily_sales_retail_0.csv');
+show tables;
